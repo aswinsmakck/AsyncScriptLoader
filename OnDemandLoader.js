1d3c1ab77ba34a9b008087e75ab4e34c25a66e31
aswinsmakck <aswinsmakck@users.noreply.github.com>
2021-06-25 11:49:13 +0530

    Add files via upload

--- OnDemandLoader.js
+++ OnDemandLoader.js
@@ -1,11 +1,14 @@
 const onDemandLoader = function () {
-
     this.require = (src, cb=null) => {
         return new Promise((resolve, reject) =>{
             setTimeout(() => {
                 let scriptFile = document.createElement('script');
                 scriptFile.src = src;
-                scriptFile.onload = cb == null ? resolve : cb;
+                //scriptFile.onload = cb == null ? resolve : cb;
+                scriptFile.onload = (evt) => {
+                    if(cb) cb();
+                    resolve(evt)
+                };
                 scriptFile.onerror = reject;
                 scriptFile.defer = true;
                 document.body.appendChild(scriptFile);
--- main.js
+++ main.js
@@ -8,7 +8,9 @@ var profile = rand > 0.5 ? profiles[1] : profiles[0];
 
 
 const addHTML = function(){
+    console.log("------------in cb ---------------")
     console.log(WDSlider)
+    console.log("------------in cb ---------------")
 }
 
 if(profile.photos.length > 4){
@@ -17,14 +19,16 @@ if(profile.photos.length > 4){
     const onDemandLoaderObj = new onDemandLoader();
     onDemandLoaderObj.require("WDSlider.js",addHTML)
     .then(response =>{
+        console.log("------------in promise ---------------")
         console.log(response);
         var WDslider = new WDSlider();
         console.log(WDslider);
+        console.log("------------in promise ---------------")
     })
     .catch(err => console.err(err))
-    onDemandLoaderObj.require("WDSlider1.js",addHTML)
+    onDemandLoaderObj.require("WDSlider1.js")
     .then(response =>{
-        console.log(response);
+        console.log("response", response);
         var WDslider = new WDSlider();
         console.log(WDslider);
     })
