2e007e6c235b40c93f74ae1ec7ebf1e8d7c22523
aswinsmakck <aswinsmakck@users.noreply.github.com>
2021-06-29 20:06:39 +0530

    Add files via upload

--- OnDemandLoader.js
+++ OnDemandLoader.js
@@ -1,18 +1,37 @@
-const onDemandLoader = function () {
-    this.require = (src, cb=null) => {
-        return new Promise((resolve, reject) =>{
-            setTimeout(() => {
-                let scriptFile = document.createElement('script');
-                scriptFile.src = src;
-                //scriptFile.onload = cb == null ? resolve : cb;
+const onDemandLoader = (function () {
+
+    const require = (src, successCb = null, errorCb = null) => {
+        let promise;
+        let scriptFile = document.createElement('script');
+        scriptFile.src = src;
+        scriptFile.defer = true;
+        document.body.appendChild(scriptFile);
+        try{
+            if(successCb){
                 scriptFile.onload = (evt) => {
-                    if(cb) cb();
-                    resolve(evt)
+                    successCb();
                 };
-                scriptFile.onerror = reject;
-                scriptFile.defer = true;
-                document.body.appendChild(scriptFile);
-            }, 0);
-        });
+                scriptFile.onerror = (evt) => {
+                    if(errorCb) errorCb();
+                    document.body.removeChild(scriptFile);
+                };
+            }
+            else{ 
+                promise =  new Promise((resolve, reject) => {
+                    scriptFile.onload = (evt) => {
+                        promise = resolve(evt);
+                    };
+                    scriptFile.onerror = (evt) => {
+                        promise = reject(evt);
+                        document.body.removeChild(scriptFile);
+                    };
+                });
+            }
+
+            return promise;
+        }catch(err){console.log(err); throw err;}
+
     }
-}
+
+    return {require : require}
+})();
--- main.js
+++ main.js
@@ -9,30 +9,46 @@ var profile = rand > 0.5 ? profiles[1] : profiles[0];
 
 const addHTML = function(){
     console.log("------------in cb ---------------")
-    console.log(WDSlider)
+    console.log(WDSlider1)
     console.log("------------in cb ---------------")
+}
+
+const errCb = () => {
+    console.log("in err cb")
 }
 
 if(profile.photos.length > 4){
     console.log(profile)
 
-    const onDemandLoaderObj = new onDemandLoader();
-    onDemandLoaderObj.require("WDSlider.js",addHTML)
-    .then(response =>{
-        console.log("------------in promise ---------------")
-        console.log(response);
-        var WDslider = new WDSlider();
-        console.log(WDslider);
-        console.log("------------in promise ---------------")
-    })
-    .catch(err => console.err(err))
-    onDemandLoaderObj.require("WDSlider1.js")
-    .then(response =>{
-        console.log("response", response);
-        var WDslider = new WDSlider();
-        console.log(WDslider);
-    })
-    .catch(err => console.err(err))
+
+    if(!window.Promise){
+        onDemandLoader.require("WDSlider.js")
+        .then(response =>{
+            console.log("------------in promise ---------------")
+            console.log(response);
+            var WDslider = new WDSlider();
+            console.log(WDslider);
+            console.log("------------in promise ---------------")
+        })
+        .catch(err => console.error(err))
+
+        onDemandLoader.require("WDSlider1.js")
+        .then(response =>{
+            console.log("response", response);
+            var WDslider = new WDSlider1();
+            console.log(WDslider);
+        })
+        .catch(err => console.error(err))
+    }
+    else{
+        try{
+            let va = onDemandLoader.require("WDSlider1.js",addHTML, errCb)
+            console.log(va);
+        }
+        catch(err){
+            console.error(err)
+        }
+    }
 }
 else{
     console.log(profile)
